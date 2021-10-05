5e240dc030c989b5e6b083c6e4a8f3b27ff94459
Necdet UYGUR <necdet.uygur@gmail.com>
2021-10-05 14:26:03 +0300

    MainPageBase

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ app = Flask(__name__)
 
 import _Kisi
 
+@app.route('/', methods=["GET"])
+def Base():
+    return """
+        /Kisi
+    """
+
 @app.after_request
 def after_request(response):
     response.headers["Access-Control-Allow-Origin"] = "*"
@@ -12,7 +18,6 @@ def after_request(response):
     response.headers["Access-Control-Allow-Headers"] = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
     response.headers["Content-Type"] = "application/json"
     return response
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8070, debug=True)
